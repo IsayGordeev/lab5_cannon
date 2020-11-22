86d5d5c60406843250a998718744bf7fc1afb876
Isay Gordeev <gordeev.ip@phystech.edu>
2020-11-23 01:54:23 +0300

    added horizontal flip, added square aim, added square shell

--- cannon.py
+++ cannon.py
@@ -13,7 +13,7 @@ SCREEN_SIZE = (800, 600)
 
 
 def rand_color():
-    return (randint(0, 255), randint(0, 255), randint(0, 255))
+    return randint(0, 255), randint(0, 255), randint(0, 255)
 
 
 class GameObject:
@@ -29,7 +29,7 @@ class Shell(GameObject):
         '''
         Constructor method. Initializes ball's parameters and initial values.
         '''
-        self.coord = coord
+        self.a = coord
         self.vel = vel
         if color == None:
             color = rand_color()
@@ -37,17 +37,17 @@ class Shell(GameObject):
         self.rad = rad
         self.is_alive = True
 
-    def check_corners(self, refl_ort=0.8, refl_par=0.9):
+    def check_corners(self, refl_ort=0.8, refl_par=0.8):
         '''
         Reflects ball's velocity when ball bumps into the screen corners. Implemetns inelastic rebounce.
         '''
         for i in range(2):
-            if self.coord[i] < self.rad:
-                self.coord[i] = self.rad
+            if self.a[i] < self.rad:
+                self.a[i] = self.rad
                 self.vel[i] = -int(self.vel[i] * refl_ort)
                 self.vel[1 - i] = int(self.vel[1 - i] * refl_par)
-            elif self.coord[i] > SCREEN_SIZE[i] - self.rad:
-                self.coord[i] = SCREEN_SIZE[i] - self.rad
+            elif self.a[i] > SCREEN_SIZE[i] - self.rad:
+                self.a[i] = SCREEN_SIZE[i] - self.rad
                 self.vel[i] = -int(self.vel[i] * refl_ort)
                 self.vel[1 - i] = int(self.vel[1 - i] * refl_par)
 
@@ -58,16 +58,67 @@ class Shell(GameObject):
         '''
         self.vel[1] += grav
         for i in range(2):
-            self.coord[i] += time * self.vel[i]
+            self.a[i] += time * self.vel[i]
         self.check_corners()
-        if self.vel[0] ** 2 + self.vel[1] ** 2 < 2 ** 2 and self.coord[1] > SCREEN_SIZE[1] - 2 * self.rad:
+        if self.vel[0] ** 2 + self.vel[1] ** 2 < 2 ** 2 and self.a[1] > SCREEN_SIZE[1] - 2 * self.rad:
             self.is_alive = False
 
     def draw(self, screen):
         '''
         Draws the ball on appropriate surface.
         '''
-        pg.draw.circle(screen, self.color, self.coord, self.rad)
+        pg.draw.circle(screen, self.color, self.a, self.rad)
+
+class Shell_type1(GameObject):
+    '''
+    The ball class. Creates a ball, controls it's movement and implement it's rendering.
+    '''
+
+    def __init__(self, coord, vel, rad=20, color=None):
+        '''
+        Constructor method. Initializes ball's parameters and initial values.
+        '''
+        adjust = 3
+        coord_new = [coord[0]-adjust , coord[1]-adjust]
+        self.a = coord_new
+        self.vel = vel
+        if color == None:
+            color = rand_color()
+        self.color = color
+        self.rad = rad
+        self.is_alive = True
+
+    def check_corners(self, refl_ort=0.8, refl_par=0.8):
+        '''
+        Reflects ball's velocity when ball bumps into the screen corners. Implemetns inelastic rebounce.
+        '''
+        for i in range(2):
+            if self.a[i] < self.rad:
+                self.a[i] = self.rad
+                self.vel[i] = -int(self.vel[i] * refl_ort)
+                self.vel[1 - i] = int(self.vel[1 - i] * refl_par)
+            elif self.a[i] > SCREEN_SIZE[i] - self.rad:
+                self.a[i] = SCREEN_SIZE[i] - self.rad
+                self.vel[i] = -int(self.vel[i] * refl_ort)
+                self.vel[1 - i] = int(self.vel[1 - i] * refl_par)
+
+    def move(self, time=1, grav=0):
+        '''
+        Moves the ball according to it's velocity and time step.
+        Changes the ball's velocity due to gravitational force.
+        '''
+        self.vel[1] += grav
+        for i in range(2):
+            self.a[i] += time * self.vel[i]
+        self.check_corners()
+        if self.vel[0] ** 2 + self.vel[1] ** 2 < 2 ** 2 and self.a[1] > SCREEN_SIZE[1] - 2 * self.rad:
+            self.is_alive = False
+
+    def draw(self, screen):
+        '''
+        Draws the ball on appropriate surface.
+        '''
+        pg.draw.polygon(screen, self.color,[(self.a[0],self.a[1]), (self.a[0]+self.rad, self.a[1]),(self.a[0]+self.rad, self.a[1]+self.rad),(self.a[0], self.a[1]+self.rad)] )
 
 
 class Cannon(GameObject):
@@ -111,18 +162,35 @@ class Cannon(GameObject):
         self.active = False
         return ball
 
+    def strike_type1(self):
+        '''
+        Creates ball, according to gun's direction and current charge power.
+        '''
+        vel = self.pow
+        angle = self.angle
+        ball = Shell_type1(list(self.coord), [int(vel * np.cos(angle)), int(vel * np.sin(angle))])
+        self.pow = self.min_pow
+        self.active = False
+        return ball
+
     def set_angle(self, target_pos):
         '''
         Sets gun's direction to target position.
         '''
         self.angle = np.arctan2(target_pos[1] - self.coord[1], target_pos[0] - self.coord[0])
 
-    def move(self, inc):
+    def move_vertical(self, inc):
         '''
         Changes vertical position of the gun.
         '''
         if (self.coord[1] > 30 or inc > 0) and (self.coord[1] < SCREEN_SIZE[1] - 30 or inc < 0):
             self.coord[1] += inc
+    def move_horizontal(self, inc):
+        '''
+        Changes horizontal position of the gun.
+        '''
+        if (self.coord[0] > 3 or inc > 0) and (self.coord[0] < SCREEN_SIZE[0] - 3 or inc < 0):
+            self.coord[0] -= inc
 
     def draw(self, screen):
         '''
@@ -161,7 +229,7 @@ class Target(GameObject):
         '''
         Checks whether the ball bumps into target.
         '''
-        dist = sum([(self.coord[i] - ball.coord[i]) ** 2 for i in range(2)]) ** 0.5
+        dist = sum([(self.coord[i] - ball.a[i]) ** 2 for i in range(2)]) ** 0.5
         min_dist = self.rad + ball.rad
         return dist <= min_dist
 
@@ -170,6 +238,45 @@ class Target(GameObject):
         Draws the target on the screen
         '''
         pg.draw.circle(screen, self.color, self.coord, self.rad)
+
+    def move(self):
+        """
+        This type of target can't move at all.
+        :return: None
+        """
+        pass
+
+class Target_type1(GameObject):
+    '''
+    Target class. Creates target, manages it's rendering and collision with a ball event.
+    '''
+
+    def __init__(self, coord=None, color=None, rad=30):
+        '''
+        Constructor method. Sets coordinate, color and radius of the target.
+        '''
+        if coord == None:
+            a = [randint(0, SCREEN_SIZE[0] - rad), randint(0, SCREEN_SIZE[0] - rad)]
+            self.a = a
+        self.rad = rad
+
+        if color == None:
+            color = rand_color()
+        self.color = color
+
+    def check_collision(self, ball):
+        '''
+        Checks whether the ball bumps into target.
+        '''
+        dist = sum([(self.a[i]+self.rad/2 - ball.a[i]) ** 2 for i in range(2)]) ** 0.5
+        min_dist = self.rad + ball.rad
+        return dist <= min_dist
+
+    def draw(self, screen):
+        '''
+        Draws the target on the screen
+        '''
+        pg.draw.polygon(screen, self.color, [(self.a[0],self.a[1]), (self.a[0]+self.rad, self.a[1]),(self.a[0]+self.rad, self.a[1]+self.rad),(self.a[0], self.a[1]+self.rad)])
 
     def move(self):
         """
@@ -188,7 +295,28 @@ class MovingTarget(Target):
     def move(self):
         self.coord[0] += self.vx
         self.coord[1] += self.vy
-        # if(self.coord[0] > )
+        if self.coord[0] < self.rad or self.coord[0] > SCREEN_SIZE[0] - self.rad:
+            self.vx *= -1
+        if self.coord[1] < self.rad or self.coord[1] > SCREEN_SIZE[1] - self.rad:
+            self.vy *= -1
+
+class MovingTarget_type1(Target_type1):
+    def __init__(self, coord=None, color=None, rad=30):
+        super().__init__(coord, color, rad)
+        self.vx = randint(-2, +2)
+        self.vy = randint(-2, +2)
+
+    def move(self):
+        self.a[0] += self.vx
+        self.a[1] += self.vy
+        if self.a[0] < self.rad or self.a[0] > SCREEN_SIZE[0] - self.rad:
+            self.vx *= -1
+        if self.a[1] < self.rad or self.a[1] > SCREEN_SIZE[1] - self.rad:
+            self.vy *= -1
+
+
+
+
 
 
 class ScoreTable:
@@ -239,6 +367,12 @@ class Manager:
         for i in range(self.n_targets):
             self.targets.append(MovingTarget(rad=randint(max(1, 30 - 2 * max(0, self.score_t.score())),
                                                          30 - max(0, self.score_t.score()))))
+        for i in range(self.n_targets):
+            self.targets.append(Target_type1(rad=randint(max(1, 30 - 2 * max(0, self.score_t.score())),
+                                                         30 - max(0, self.score_t.score()))))
+        for i in range(self.n_targets):
+            self.targets.append(MovingTarget_type1(rad=randint(max(1, 30 - 2 * max(0, self.score_t.score())),
+                                                         30 - max(0, self.score_t.score()))))
 
     def process(self, events, screen):
         '''
@@ -254,7 +388,7 @@ class Manager:
         self.collide()
         self.draw(screen)
 
-        if len(self.targets) == 0 and len(self.balls) == 0:
+        if len(self.targets) == 0 :
             self.new_mission()
 
         return done
@@ -269,15 +403,22 @@ class Manager:
                 done = True
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_UP:
-                    self.gun.move(-5)
+                    self.gun.move_vertical(-5)
                 elif event.key == pg.K_DOWN:
-                    self.gun.move(5)
+                    self.gun.move_vertical(5)
+                if event.key == pg.K_RIGHT:
+                    self.gun.move_horizontal(-5)
+                elif event.key == pg.K_LEFT:
+                    self.gun.move_horizontal(5)
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     self.gun.activate()
             elif event.type == pg.MOUSEBUTTONUP:
                 if event.button == 1:
-                    self.balls.append(self.gun.strike())
+                    if self.score_t.b_used%2 == 0:
+                        self.balls.append(self.gun.strike())
+                    else:
+                        self.balls.append(self.gun.strike_type1())
                     self.score_t.b_used += 1
         return done
 
@@ -325,7 +466,7 @@ class Manager:
 
 
 screen = pg.display.set_mode(SCREEN_SIZE)
-pg.display.set_caption("The gun of Gordeev")
+pg.display.set_caption("The gun")
 
 done = False
 clock = pg.time.Clock()
